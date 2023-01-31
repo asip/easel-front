f4196929446966523408364c8892471460db120c
Yasumasa Ashida <ys.ashida@gmail.com>
2023-01-31 19:39:48 +0900

    refactor: refactoring of composables/use_frame_search

--- composables/use_frame_search.ts
+++ composables/use_frame_search.ts
@@ -1,4 +1,5 @@
 import { cdate } from 'cdate'
+import { Frame } from '~/composables/use_frame'
 
 export const useFrameSearch = () => {
   const frame_query = useState('frame_query', () => {
@@ -9,7 +10,7 @@ export const useFrameSearch = () => {
     }
   })
 
-  const frames = useState('frames', () => { return [] })
+  const frames = useState<Frame[]>('frames', () => { return [] })
 
   const date_word = computed({
     get(){
@@ -45,7 +46,7 @@ export const useFrameSearch = () => {
         frames.value.splice(0, frames.value.length);
         for (let frame of frame_list as []) {
           //console.log(comment);
-          frames.value.push(frame);
+          frames.value.push(createJson2Frame(frame));
         }
         //console.log(frames)
       }
@@ -53,6 +54,40 @@ export const useFrameSearch = () => {
         frame_query.value.pages = json_data.meta.pagination.pages
       }
     }
+  }
+
+  const createJson2Frame = (row_data: any) : Frame => {
+    const frame: Frame = {
+      id: null,
+      user_id: null,
+      user_name: '',
+      name: '',
+      tag_list: '',
+      tags: [],
+      comment: '',
+      shooted_at: '',
+      shooted_at_html: '',
+      file: null,
+      file_url: '',
+      file_two_url: '',
+      file_three_url: '',
+      preview_url: null,
+      updated_at: ''
+    }
+    frame.id = row_data.id
+    frame.user_id = row_data.attributes.user_id
+    frame.user_name = row_data.attributes.user_name
+    frame.name = row_data.attributes.name
+    frame.comment = row_data.attributes.comment
+    frame.tag_list = row_data.attributes.tag_list
+    frame.tags = row_data.attributes.tags
+    frame.shooted_at = row_data.attributes.shooted_at
+    frame.shooted_at_html = row_data.attributes.shooted_at_html
+    frame.updated_at = row_data.attributes.updated_at
+    frame.file_url = row_data.attributes.file_url
+    frame.file_two_url = row_data.attributes.file_two_url
+    frame.file_three_url = row_data.attributes.file_three_url
+    return frame
   }
 
   return {
