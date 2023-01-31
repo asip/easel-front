f4dec5caa8954e63c7ac5ce2252403558f4a9621
Yasumasa Ashida <ys.ashida@gmail.com>
2023-01-31 19:40:11 +0900

    refactor: refactoring of composables/use_frame_search

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
   <br>
   <div class="row col-sm-12">
     <div v-for="frame in frames" class="card col-sm-3 kadomaru">
-      <NuxtLink :to="`${baseURL}${frame.attributes.file_url}`" name="lm" class="mx-auto" style="padding-top: 10px;"><img :src="frame.attributes.file_two_url" class="card-img-top"></NuxtLink>
+      <NuxtLink :to="`${baseURL}${frame.file_url}`" name="lm" class="mx-auto" style="padding-top: 10px;"><img :src="frame.file_two_url" class="card-img-top"></NuxtLink>
       <br>
       <div class="card-block">
         <div class="d-flex justify-content-sm-center">
           <div class="mx-auto" style="padding-bottom: 10px;">
-            <NuxtLink :to="`/frames/${frame.id}`" class="mx-auto">{{frame.attributes.name}}</NuxtLink>
+            <NuxtLink :to="`/frames/${frame.id}`" class="mx-auto">{{frame.name}}</NuxtLink>
           </div>
         </div>
       </div>
