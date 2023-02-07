56ab1ad71c1fd8cf7225396f7ae529750fd0372e
Yasumasa Ashida <ys.ashida@gmail.com>
2023-02-07 23:36:01 +0900

    feat: edit nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr:true,
+  app: {
+    head: {
+      title: "Easel",
+      meta: [
+        {charset: "utf-8"},
+        {name: "viewport", content: "width=device-width, initial-scale=1"},
+      ],
+      link: [
+        { href: "/palette.svg", type: "image/svg+xml", rel: "icon" },
+        { href: "/palette.svg", type: "image/svg+xml", rel: "mask-icon", color: "#000000" }
+      ]
+    }
+  },
   css: [
     "~/assets/styles/main.scss",
     "bootstrap-icons/font/bootstrap-icons.css"
