ce7d5fc83bb8c1bac81519ebd4c6177e8dd8ae62
Yasumasa Ashida <ys.ashida@gmail.com>
2023-02-26 09:12:26 +0900

    refactor: edit components/frame_comments

--- components/frame_comments.vue
+++ components/frame_comments.vue
@@ -9,17 +9,13 @@
   import { Frame } from '~/composables/use_frame'
 
   const commenter = useComment();
+  const { comment, getComments } = commenter
   const frame: Frame | undefined = inject('frame')
 
   provide('commenter', commenter)
 
-  commenter.comment.frame_id = frame?.id
+  comment.frame_id = frame?.id
 
   //console.log(comment.frame_id);
-  await commenter.getComments()
-
-  const updateComments = async () => {
-    commenter.comments.splice(0, commenter.comments.length);
-    await commenter.getComments()
-  }
+  await getComments()
 </script>
