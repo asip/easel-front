0f4f94f289a2a64bea74d4b69931457144240ab7
Yasumasa Ashida <ys.ashida@gmail.com>
2023-03-05 21:20:47 +0900

    refactor: edit pages/login

---
+++ components/users/login/google.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="googleClientID !== ''" id="googleButton"></div>
+</template>
+
+<script setup lang="ts">
+  const { googleClientID } = useConstants()
+  const { login_with_google } = useLoginUser()
+
+  onMounted(() => {
+    google.accounts.id.initialize({
+      client_id: googleClientID,
+      callback: handleCredentialResponse, //method to run after user clicks the Google sign in button
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("googleButton"),
+      { theme: "outline", size: "large" } // customization attributes
+    );
+    google.accounts.id.prompt(); // also display the One Tap dialog
+  })
+
+  const handleCredentialResponse = async (response: any) => {
+    // call your backend API here
+    // the token can be accessed as: response.credential
+    await login_with_google(response)
+
+    navigateTo('/')
+  }
+</script>
