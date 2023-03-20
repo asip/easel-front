c25456c4d9966424d00ebc4034fbf0eac3cbbab7
Yasumasa Ashida <ys.ashida@gmail.com>
2023-03-20 13:44:33 +0900

    feat: edit components/tag_search

--- components/tag_search.vue
+++ components/tag_search.vue
@@ -9,7 +9,7 @@
     </div>
   </form>
   <teleport to="#sidebar-calendar">
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvas-calendar" aria-labelledby="offcanvasCalendarLabel">
+    <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvas-calendar" aria-labelledby="offcanvasCalendarLabel">
       <div class="offcanvas-header">
         <h5 class="offcanvas-title" id="offcanvasLabel"></h5>
         <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" :aria-label="$t('action.modal.close')"></button>
