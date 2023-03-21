ce6478518c307805d0331fc7190f63b7b42129d6
Yasumasa Ashida <ys.ashida@gmail.com>
2023-03-21 17:10:28 +0900

    style(css&html): edit components/users

--- components/users/new/input.vue
+++ components/users/new/input.vue
@@ -1,63 +1,85 @@
 <template>
   <div class="row d-flex justify-content-sm-center">
-    <label for="image" class="col-form-label col-sm-3 label-bg-style">{{ $t('model.user.image') }}：</label>
-    <div class="form-group col-sm-4">
-      <input type="file" accept="image/jpg,image/jpeg,image/png" multiple="false" @change="onSelectFile" class="form-control-file" >
-      <div v-for="message of error_messages.image">
-        <div>{{ message }}</div>
-      </div>
+    <div class="col-sm-7">
+      <table class="table table-bordered table_rounded">
+        <tbody>
+          <tr>
+            <td style="width: 25%;">
+              <label for="image" class="col-form-label">{{ $t('model.user.image') }}：</label>
+            </td>
+            <td style="width: 75%;">
+              <input type="file" accept="image/jpg,image/jpeg,image/png" multiple="false" @change="onSelectFile" class="form-control-file" >
+              <div v-for="message of error_messages.image">
+                <div>{{ message }}</div>
+              </div>
+            </td>
+          </tr>
+          <tr v-if="signup_params.image !== null">
+            <td colspan="2">
+              <ImagePreview />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="name" class="col-form-label">{{ $t('model.user.name') }}：</label>
+            </td>
+            <td>
+              <input type="text" v-model="signup_params.name" :placeholder="$t('model.user.name')" class="form-control">
+              <div v-for="error of v$.name.$errors" :key="error.$uid">
+                <div>{{ error.$message }}</div>
+              </div>
+              <div v-for="message of error_messages.name">
+                <div>{{ $t('model.user.name') }}{{ message }}</div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="email" class="col-form-label">{{ $t('model.user.email') }}：</label>
+            </td>
+            <td>
+              <input type="text" v-model="signup_params.email" :placeholder="$t('model.user.email')" class="form-control" >
+              <div v-for="error of v$.email.$errors" :key="error.$uid">
+                <div>{{ error.$message }}</div>
+              </div>
+              <div v-for="message of error_messages.email">
+                <div>{{ $t('model.user.email') }}{{ message }}</div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="password" class="col-form-label">{{ $t('model.user.password') }}：</label>
+            </td>
+            <td>
+              <input type="password" v-model="signup_params.password" :placeholder="$t('model.user.password')" class="form-control">
+              <div v-for="error of v$.password.$errors" :key="error.$uid">
+                <div>{{ error.$message }}</div>
+              </div>
+              <div v-for="message of error_messages.password">
+                <div>{{ $t('model.user.password') }}{{ message }}</div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <label for="password_confirmation" class="col-form-label">{{ $t('model.user.password_confirmation') }}：</label>
+            </td>
+            <td>
+              <input type="password" v-model="signup_params.password_confirmation" :placeholder="$t('model.user.password_confirmation')" class="form-control">
+              <div v-for="error of v$.password_confirmation.$errors" :key="error.$uid">
+                <div>{{ error.$message }}</div>
+              </div>
+              <div v-for="message of error_messages.password_confirmation">
+                <div>{{ $t('model.user.password_confirmation') }}{{ message }}</div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+    <br>
   </div>
-  <ImagePreview />
-  <div class="row d-flex justify-content-sm-center border border-white">
-    <label for="name" class="col-form-label col-sm-3 label-bg-style">{{ $t('model.user.name') }}：</label>
-    <div class="form-group col-sm-4">
-      <input type="text" v-model="signup_params.name" :placeholder="$t('model.user.name')" class="form-control">
-      <div v-for="error of v$.name.$errors" :key="error.$uid">
-        <div>{{ error.$message }}</div>
-      </div>
-      <div v-for="message of error_messages.name">
-        <div>{{ $t('model.user.name') }}{{ message }}</div>
-      </div>
-    </div>
-  </div>
-  <div class="row d-flex justify-content-sm-center border border-white">
-    <label for="email" class="col-form-label col-sm-3 label-bg-style">{{ $t('model.user.email') }}：</label>
-    <div class="form-group col-sm-4">
-      <input type="text" v-model="signup_params.email" :placeholder="$t('model.user.email')" class="form-control" >
-      <div v-for="error of v$.email.$errors" :key="error.$uid">
-        <div>{{ error.$message }}</div>
-      </div>
-      <div v-for="message of error_messages.email">
-        <div>{{ $t('model.user.email') }}{{ message }}</div>
-      </div>
-    </div>
-  </div>
-  <div class="row d-flex justify-content-sm-center border border-white">
-    <label for="password" class="col-form-label col-sm-3 label-bg-style">{{ $t('model.user.password') }}：</label>
-    <div class="form-group col-sm-4">
-      <input type="password" v-model="signup_params.password" :placeholder="$t('model.user.password')" class="form-control">
-      <div v-for="error of v$.password.$errors" :key="error.$uid">
-        <div>{{ error.$message }}</div>
-      </div>
-      <div v-for="message of error_messages.password">
-        <div>{{ $t('model.user.password') }}{{ message }}</div>
-      </div>
-    </div>
-  </div>
-  <div class="row d-flex justify-content-sm-center border border-white">
-    <label for="password_confirmation" class="col-form-label col-sm-3 label-bg-style">{{ $t('model.user.password_confirmation') }}：</label>
-    <div class="form-group col-sm-4">
-      <input type="password" v-model="signup_params.password_confirmation" :placeholder="$t('model.user.password_confirmation')" class="form-control">
-      <div v-for="error of v$.password_confirmation.$errors" :key="error.$uid">
-        <div>{{ error.$message }}</div>
-      </div>
-      <div v-for="message of error_messages.password_confirmation">
-        <div>{{ $t('model.user.password_confirmation') }}{{ message }}</div>
-      </div>
-    </div>
-  </div>
-  <br>
   <div class="d-flex justify-content-sm-center">
     <div class="form-group col-sm-6">
       <button type="button" @click="onSignupClick" class="btn btn-primary">{{ $t('action.model.create') }}</button>&nbsp;
