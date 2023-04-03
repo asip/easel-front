58bc6f37477413303d10d1b1c8349ee41bf331dd
Yasumasa Ashida <ys.ashida@gmail.com>
2023-04-03 14:53:46 +0900

    design: change the look and feel of components/tag_search

--- components/tag_search.vue
+++ components/tag_search.vue
@@ -1,13 +1,11 @@
 <template>
-  <form class="d-flex">
-    <input type="text" v-model="frame_query.word" :placeholder="$t('component.tag_search.placeholder')" class="form-control me-sm-2" >
-    <input type="button" :value="$t('component.tag_search.search')" class="btn btn-outline-success me-2 me-sm-0" @click="onSearchClick">
+  <div class="d-flex">
     <div style="padding-left:5px;">
       <a class="btn btn-outline-success me-2 me-sm-0" data-bs-toggle="offcanvas" href="#offcanvas-calendar" role="button" aria-expanded="false" aria-controls="offcanvas-calendar">
-        <i class="bi bi-calendar"></i>
+        <i class="bi bi-search"></i>
       </a>
     </div>
-  </form>
+  </div>
   <teleport to="#sidebar-calendar">
     <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvas-calendar" aria-labelledby="offcanvasCalendarLabel">
       <div class="offcanvas-header">
@@ -20,6 +18,15 @@
             <client-only>
               <v-date-picker v-model="date_word" :masks="masks" :locale="locale" />
             </client-only>
+          </div>
+        </div>
+        <br>
+        <div class="d-flex justify-content-sm-center">
+          <div class="mx-auto">
+            <form class="d-flex">
+            <input type="text" v-model="frame_query.word" :placeholder="$t('component.tag_search.placeholder')" class="form-control me-sm-2" >
+            <input type="button" :value="$t('component.tag_search.search')" class="btn btn-outline-success me-2 me-sm-0" @click="onSearchClick">
+            </form>
           </div>
         </div>
       </div>
