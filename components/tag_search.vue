4d126a60e958d5a494c856d1e641a6438b5999dd
Yasumasa Ashida <ys.ashida@gmail.com>
2023-04-05 10:17:11 +0900

    design: edit components/tag_search

--- components/tag_search.vue
+++ components/tag_search.vue
@@ -1,38 +1,36 @@
 <template>
-
-    <div style="padding-left:5px;">
-      <a class="btn btn-outline-success me-2 me-sm-0" data-bs-toggle="offcanvas" href="#offcanvas-calendar"
-         role="button" aria-expanded="false" aria-controls="offcanvas-calendar">
-        <i class="bi bi-search"></i>
-      </a>
-    </div>
-    <teleport to="#sidebar-calendar">
-      <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvas-calendar"
-           aria-labelledby="offcanvasCalendarLabel">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasLabel"></h5>
-          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                  :aria-label="$t('action.modal.close')"></button>
-        </div>
-        <div class="offcanvas-body">
-          <div class="d-flex justify-content-sm-center">
-            <client-only>
+  <div style="padding-left:5px;">
+    <a class="btn btn-outline-success me-2 me-sm-0" data-bs-toggle="offcanvas" href="#offcanvas-calendar"
+       role="button" aria-expanded="false" aria-controls="offcanvas-calendar">
+      <i class="bi bi-search"></i>
+    </a>
+  </div>
+  <teleport to="#sidebar-calendar">
+    <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvas-calendar"
+         aria-labelledby="offcanvasCalendarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasLabel"></h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                :aria-label="$t('action.modal.close')"></button>
+      </div>
+      <div class="offcanvas-body">
+        <div class="d-flex justify-content-sm-center">
+          <client-only>
             <v-date-picker v-model="date_word" :masks="masks" :locale="locale"/>
-            </client-only>
-          </div>
-          <br>
-          <div class="d-flex justify-content-sm-center">
-            <form class="d-flex">
-              <input type="text" v-model="frame_query.word" :placeholder="$t('component.tag_search.placeholder')"
-                     class="form-control me-sm-2">
-              <input type="button" :value="$t('component.tag_search.search')"
-                     class="btn btn-outline-success me-2 me-sm-0" @click="onSearchClick">
-            </form>
-          </div>
+          </client-only>
+        </div>
+        <br>
+        <div class="d-flex justify-content-sm-center">
+          <form class="d-flex">
+            <input type="text" v-model="frame_query.word" :placeholder="$t('component.tag_search.placeholder')"
+                   class="form-control me-sm-2">
+            <input type="button" :value="$t('component.tag_search.search')"
+                   class="btn btn-outline-success me-2 me-sm-0" @click="onSearchClick">
+          </form>
         </div>
       </div>
-    </teleport>
-
+    </div>
+  </teleport>
 </template>
 
 <script lang="ts" setup>
