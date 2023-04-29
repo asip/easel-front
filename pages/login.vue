d30e9b381fa6204e3fd28548b9daaba6e600ff02
Yasumasa Ashida <ys.ashida@gmail.com>
2023-04-29 23:54:04 +0900

    design: edit pages/login

--- pages/login.vue
+++ pages/login.vue
@@ -12,15 +12,27 @@
       <div class="card-block">
         <br>
         <div class="row d-flex justify-content-sm-center border border-white">
-          <label for="email" class="col-form-label-sm col-sm-2 label-bg-style">{{ $t('model.user.email') }}</label>
-          <div class="form-group col-sm-4">
-            <input type="text" v-model="login_params.email" :placeholder="$t('model.user.email')" class="form-control">
-          </div>
-        </div>
-        <div class="row d-flex justify-content-sm-center border border-white">
-          <label for="password" class="col-form-label-sm col-sm-2 label-bg-style">{{ $t('model.user.password') }}</label>
-          <div class="form-group col-sm-4">
-            <input type="password" v-model="login_params.password" :placeholder="$t('model.user.password')" class="form-control">
+          <div class="col-sm-6">
+            <table class="table table-bordered table_rounded">
+              <tbody>
+                <tr>
+                  <td style="width: 30%;">
+                    <label for="email" class="col-form-label-sm">{{ $t('model.user.email') }}</label>
+                  </td>
+                  <td style="width: 70%;">
+                    <input type="text" v-model="login_params.email" :placeholder="$t('model.user.email')" class="form-control">
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <label for="password" class="col-form-label-sm">{{ $t('model.user.password') }}</label>
+                  </td>
+                  <td>
+                    <input type="password" v-model="login_params.password" :placeholder="$t('model.user.password')" class="form-control">
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
         <div class="row d-flex justify-content-sm-center">
@@ -31,6 +43,7 @@
         <br>
         <div class="row d-flex justify-content-sm-center">
           <div class="form-group col-sm-6">
+            &nbsp;
             <button type="button" class="btn btn-primary" @click="onLoginClick">{{ $t('action.user.login') }}</button>&nbsp;
             <NuxtLink :to="`/signup`" class="btn btn-primary">{{ $t('action.user.new') }}</NuxtLink>&nbsp;
             <NuxtLink :to="`/`" class="btn btn-outline-secondary">{{ $t('action.model.return') }}</NuxtLink>
