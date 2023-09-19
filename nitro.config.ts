36889da9bf085e1fcd95aa97750d11cb636cbfb2
Yasumasa Ashida <ys.ashida@gmail.com>
2023-09-19 20:06:25 +0900

    chore: edit nitro.config.ts

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,26 +3,4 @@ import {defineNitroConfig} from 'nitropack/config'
 // https://nitro.unjs.io/config#devproxy
 // https://github.com/http-party/node-http-proxy#options
 export default defineNitroConfig({
-  devProxy: {
-    '/api/': {
-      target: 'http://localhost:3000/api/v1/',
-      changeOrigin: true,
-      // hostRewrite: ,
-      // cookieDomainRewrite: ,
-      headers: {
-        'X-Forwarded-Host': 'localhost:3000',
-        'X-Forwarded-Proto': 'http'
-      },
-    },
-    '/uploads/': {
-      target: 'http://localhost:3000/uploads/',
-      changeOrigin: true,
-      // hostRewrite: ,
-      // cookieDomainRewrite: ,
-      headers: {
-        'X-Forwarded-Host': 'localhost:3000',
-        'X-Forwarded-Proto': 'http'
-      },
-    }
-  }
 })
