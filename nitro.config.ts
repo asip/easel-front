a9c189a7650f300925929f5347db1b4a9989f682
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-04 10:36:25 +0900

    chore: remove nitro.config.ts

--- nitro.config.ts
+++
@@ -1,6 +0,0 @@
-import {defineNitroConfig} from 'nitropack/config'
-
-// https://nitro.unjs.io/config#devproxy
-// https://github.com/http-party/node-http-proxy#options
-export default defineNitroConfig({
-})
