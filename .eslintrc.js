8dfad08be75f1078c3f65526ec041e5b8924368e
Yasumasa Ashida <ys.ashida@gmail.com>
2023-10-27 09:08:27 +0900

    chore: change extension of eslintrc from json to js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    "@nuxtjs/eslint-config-typescript"
+  ],
+  rules: {
+    camelcase: 0,
+    "vue/multi-word-component-names": 0,
+    "vue/no-v-html": 0
+  }
+}
