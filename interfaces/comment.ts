f8691b97db5916f21a72f9cd84d591cdbe4354f4
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-18 15:45:25 +0900

    refactor(composable): refactoring of use_comment

--- interfaces/comment.ts
+++ interfaces/comment.ts
@@ -1,9 +1,9 @@
 export interface Comment {
-  id: number
-  frame_id: number | null | undefined
-  body: string
-  user_id: number | null
-  user_name: string
-  user_image_url: string
-  updated_at: string | null
+  id?: number
+  frame_id?: number | null | undefined
+  body?: string
+  user_id?: number | null
+  user_name?: string
+  user_image_url?: string
+  updated_at?: string | null
 }
