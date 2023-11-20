fa93cdad61203dd172976938e8664786f1cf4eb4
Yasumasa Ashida <ys.ashida@gmail.com>
2023-11-20 21:58:11 +0900

    chore: update nuxt.config.js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ export default defineNuxtConfig({
       redirectOn: 'root'// recommended
     },
     strategy: 'no_prefix',
-    langDir: 'lang',
+    langDir: 'locales',
     locales: [
       {
         code: 'en',
