ba17d66362cf2bf1d738e19fde045c057a776e08
Yasumasa Ashida <ys.ashida@gmail.com>
2023-12-09 22:47:57 +0900

    chore: edit eslint config

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat
+
+export default [
+  ...compat.config(
+    {
+      extends: [
+        '@nuxtjs/eslint-config-typescript'
+      ],
+      rules: {
+        camelcase: 0,
+        'import/default': 0,
+        'import/namespace': 0,
+        'import/no-named-as-default': 0,
+        'import/no-named-as-default-member': 0,
+        'vue/multi-word-component-names': 0,
+        'vue/no-v-html': 0
+      }
+    }
+  )
+]
