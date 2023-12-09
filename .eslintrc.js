a660739097f593bdc0683dca57fade687958837d
Yasumasa Ashida <ys.ashida@gmail.com>
2023-12-09 15:49:09 +0900

    chore: edit .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
   root: true,
   extends: [
-    "@nuxtjs/eslint-config-typescript"
+    '@nuxtjs/eslint-config-typescript'
   ],
   rules: {
     camelcase: 0,
-    "vue/multi-word-component-names": 0,
-    "vue/no-v-html": 0
+    'vue/multi-word-component-names': 0,
+    'vue/no-v-html': 0
   }
 }
