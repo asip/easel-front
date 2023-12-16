239fcee32ed9c7459f9859029f16bf0f35ad63e4
Yasumasa Ashida <ys.ashida@gmail.com>
2023-12-16 17:39:37 +0900

    fix(page): fix index

--- pages/index.vue
+++ pages/index.vue
@@ -60,13 +60,21 @@ const clickCallback = async (pageNum: number) => {
   await searchFrame()
 }
 
-onUpdated(() => {
+const initGallery = () => {
   if (gallery) { gallery.destroy() }
   const elements: NodeListOf<Element> = document.querySelectorAll('.lum-lightbox')
   Array.from(elements).forEach(e => e.remove())
 
   const elms = document.querySelectorAll('[name="lm"]')
   gallery = new LuminousGallery(elms, { showCloseButton: true })
+}
+
+onMounted(() => {
+  initGallery()
+})
+
+onUpdated(() => {
+  initGallery()
 })
 
 onUnmounted(() => {
