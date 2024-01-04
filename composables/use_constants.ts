d42d97c6d7b46dc0aa069434e5a65b12d3daf3ec
Yasumasa Ashida <ys.ashida@gmail.com>
2024-01-04 15:44:35 +0900

    refactor(composable): edit use_constants

--- composables/use_constants.ts
+++ composables/use_constants.ts
@@ -17,5 +17,5 @@ export const useConstants = () => {
 
   const googleClientID: string = runtimeConfig.public.googleClientId
 
-  return { backendOriginURL, backendApiURL, googleClientID }
+  return { backendApiURL, googleClientID }
 }
