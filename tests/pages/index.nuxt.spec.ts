454507087c8ef3b6cc399c1b6b013108424454cb
Yasumasa Ashida <ys.ashida@gmail.com>
2024-02-05 13:42:34 +0900

    refactor(test): edit components/index

--- tests/pages/index.nuxt.spec.ts
+++ tests/pages/index.nuxt.spec.ts
@@ -1,12 +1,11 @@
 import { describe, test } from 'vitest'
-import { renderSuspended } from '@nuxt/test-utils/runtime'
+import { mountSuspended } from '@nuxt/test-utils/runtime'
 import Index from '~/pages/index.vue'
 
 describe('Index', () => {
   test('display', async () => {
     // Arrange
-    //const { container } = 
-    await renderSuspended(Index)
+    //const { container } =
+    await mountSuspended(Index)
   })
 })
-
