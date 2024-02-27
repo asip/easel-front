06912fa86abbaff31b698f2acf73f8f1d17c245b
Yasumasa Ashida <ys.ashida@gmail.com>
2024-02-27 12:46:08 +0900

    refactor(util): edit validators/objects/*

--- utils/validators/objects/tagArrayLength.ts
+++ utils/validators/objects/tagArrayLength.ts
@@ -3,7 +3,7 @@ import tagArrayLength from '../raw/tagArrayLength'
 export default function (size: number) {
   return {
     $validator: tagArrayLength(size),
-    $message: ({ $params }: { $params: any }) => `are limited to ${$params.size}.`,
+    $message: ({ $params }: { $params: { size: number } }) => `are limited to ${$params.size}.`,
     $params: { size, type: 'tagArrayLength' }
   }
 }
--- utils/validators/objects/tagLength.ts
+++ utils/validators/objects/tagLength.ts
@@ -3,7 +3,7 @@ import tagLength from '../raw/tagLength'
 export default function (size: number) {
   return {
     $validator: tagLength(size),
-    $message: ({ $params }: { $params: any }) => `are limited to ${$params.size} characters.`,
+    $message: ({ $params }: { $params: { size: number } }) => `are limited to ${$params.size} characters.`,
     $params: { size, type: 'tagLength' }
   }
 }
