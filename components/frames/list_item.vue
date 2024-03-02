43c1f384d26a9d120c3c742a62296801ce8843ad
Yasumasa Ashida <ys.ashida@gmail.com>
2024-03-02 09:15:26 +0900

    refactor(page): edit index

--- components/frames/list_item.vue
+++ components/frames/list_item.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="card-block" style="padding-top: 10px;padding-bottom: 10px;">
-    <NuxtLink :to="`${frame.file_url}`" name="lm" class="mx-auto">
-      <img :src="`${frame.file_two_url}`" :alt="frame.name" class="card-img-top">
+    <NuxtLink :to="`${frame?.file_url}`" name="lm" class="mx-auto">
+      <img :src="`${frame?.file_two_url}`" :alt="frame?.name" class="card-img-top">
     </NuxtLink>
     <br>
     <div class="d-flex justify-content-sm-center">
-      <NuxtLink :to="`/frames/${frame.id}`" class="mx-auto">
-        {{ frame.name }}
+      <NuxtLink :to="`/frames/${frame?.id}`" class="mx-auto">
+        {{ frame?.name }}
       </NuxtLink>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { Frame } from '~/interfaces/frame'
+
 const frame = defineModel<Frame>()
 </script>
