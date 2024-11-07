60f3fa0776a3ed19836debb7f5bde5603e567958
Yasumasa Ashida <ys.ashida@gmail.com>
2024-11-07 14:41:04 +0900

    [wip] test(page): change the structure

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineVitestConfig({
   // any custom Vitest config you require
   test: {
     environment: 'nuxt',
+    dir: 'tests',
     env: {
       BACKEND_ORIGIN_URL: 'http://localhost:3000/api/v1'
     }
