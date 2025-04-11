c313c0fe047ceb1ef26aad7c8220c2c4057b5ec8
Yasumasa Ashida <ys.ashida@gmail.com>
2025-04-11 10:27:37 +0900

    refactor(composable): refactoring of getSignupRules & getUserRules

--- app/composables/validation/forms/get-user-rules.ts
+++ app/composables/validation/forms/get-user-rules.ts
@@ -1,24 +1,20 @@
 import type { User } from '~/interfaces/user'
 import { required, minLength, maxLength, email, sameAs } from '@regle/rules'
 
-export function getUserRules (user: User) {
-  const userRules = computed(() => {
-    return {
-      name: {
-        required: withMessage(required, () => i18n.global.t('rules.required')),
-        minLength: withMessage(minLength(3), () => i18n.global.t('rules.minLength', { min: 3 })),
-        maxLength: withMessage(maxLength(40),() => i18n.global.t('rules.maxLength', { max: 40 }))
-      },
-      email: {
-        required: withMessage(required, () => i18n.global.t('rules.required')),
-        email: withMessage(email, () => i18n.global.t('rules.email'))
-      },
-      password: {},
-      password_confirmation: {
-        sameAs: withMessage(sameAs(user.password), () => i18n.global.t('rules.sameAs'))
-      }
+export const getUserRules = (user: User) => {
+  return {
+    name: {
+      required: withMessage(required, () => i18n.global.t('rules.required')),
+      minLength: withMessage(minLength(3), () => i18n.global.t('rules.minLength', { min: 3 })),
+      maxLength: withMessage(maxLength(40),() => i18n.global.t('rules.maxLength', { max: 40 }))
+    },
+    email: {
+      required: withMessage(required, () => i18n.global.t('rules.required')),
+      email: withMessage(email, () => i18n.global.t('rules.email'))
+    },
+    password: {},
+    password_confirmation: {
+      sameAs: withMessage(sameAs(user.password), () => i18n.global.t('rules.sameAs'))
     }
-  })
-
-  return userRules
+  }
 }
