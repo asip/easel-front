9b2310d352ff91fe76f9abc544bf8b2194b4dee4
Yasumasa Ashida <ys.ashida@gmail.com>
2025-04-22 13:42:56 +0900

    efactor(composable): refactoring of data fetching

--- app/plugins/api.ts
+++ app/plugins/api.ts
@@ -2,7 +2,6 @@ export default defineNuxtPlugin((nuxtApp) => {
   const api = $fetch.create({
     baseURL: useConstants().backendApiURL.value
   })
-  // useNuxtApp().$api で使用可能にする
   return {
     provide: {
       api,
