449fdfba73f9e5f58d89f19229529ad05c12ad33
Yasumasa Ashida <ys.ashida@gmail.com>
2025-04-23 18:33:06 +0900

    refactor(plugin): edit v-calendar.client.ts

--- app/plugins/v-calendar.client.ts
+++ app/plugins/v-calendar.client.ts
@@ -1,5 +1,5 @@
 import VCalendar from 'v-calendar'
-import 'v-calendar/style.css'
+import 'v-calendar/dist/style.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VCalendar, {})
