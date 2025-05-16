b47589833de55cc1f5dad004bed8eeb13baa0cc5
Yasumasa Ashida <ys.ashida@gmail.com>
2025-05-16 23:11:49 +0900

    chore: switch from sorcery to devise

--- app/composables/validation/forms/get-password-rules.ts
+++ app/composables/validation/forms/get-password-rules.ts
@@ -1,9 +1,9 @@
 import type { User } from '~/interfaces/user'
-import { required, maxLength, sameAs } from '@regle/rules'
+import { required, minLength, maxLength, sameAs } from '@regle/rules'
 
 export const getPasswordRules = (user: User) =>
   () => ({
     current_password: { required },
-    password: { maxLength: maxLength(128) },
+    password: { minLength: minLength(6), maxLength: maxLength(128) },
     password_confirmation: { sameAs: sameAs(user.password) }
   })
