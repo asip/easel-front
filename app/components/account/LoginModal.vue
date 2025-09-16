6b4bca9b781946fd6d4579356b8d3691beb95ebc
Yasumasa Ashida <ys.ashida@gmail.com>
2025-09-16 09:26:17 +0900

    design(component): Fix account/LoginModal

--- app/components/account/LoginModal.vue
+++ app/components/account/LoginModal.vue
@@ -28,25 +28,24 @@ const onCloseClick = () => {
         <div>
         {{ $t('action.user.login') }}
         </div>
-        <div/>
         <a href="#" @click.prevent="onCloseClick">
           <i class="bi bi-x-circle text-accent hover:text-primary" />
         </a>
       </div>
       <div>
-      <div class="flex justify-center border border-white gap-1 mb-1">
-        <AccountLoginGoogle v-if="!loggedIn" />
-        <button
-          class="btn btn-outline btn-primary"
-          @click="onSignupClick"
-        >
-          <i class="bi bi-person-plus-fill" />{{ $t('action.user.new') }}
-        </button>
+        <div class="flex justify-center border border-white gap-1 mb-1">
+          <AccountLoginGoogle v-if="!loggedIn" />
+          <button
+            class="btn btn-outline btn-primary"
+            @click="onSignupClick"
+          >
+            <i class="bi bi-person-plus-fill" />{{ $t('action.user.new') }}
+          </button>
+        </div>
+        <div class="flex justify-center border border-white">
+          <AccountLoginForm ref="form" />
+        </div>
       </div>
-      <div class="flex justify-center border border-white">
-        <AccountLoginForm ref="form" />
-      </div>
-    </div>
     </div>
   </dialog>
 </template>
