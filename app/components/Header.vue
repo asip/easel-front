9b2f5361ad4f1bff4cdd4677747ac10dc4c4f61a
Yasumasa Ashida <ys.ashida@gmail.com>
2025-12-15 11:58:03 +0900

    fix: Fix the behavior of the logout button in the header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -26,6 +26,7 @@ const onLogoutClick = async (): Promise<void> => {
   } else if (route.path.match(/^\/frames\/\d+\/edit$/)) {
     await navigateTo(`/frames/${route.params.id}`)
   }
+  location.reload()
 }
 
 const onTopPageClick = async (): Promise<void> => {
