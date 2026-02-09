950d5e829d6fafddf74e64de8641ed07ef1964c3
Yasumasa Ashida <ys.ashida@gmail.com>
2026-02-09 19:39:38 +0900

    refactor(composable): Refactor ui/use-image-preview

--- app/composables/ui/use-image-preview.ts
+++ app/composables/ui/use-image-preview.ts
@@ -15,18 +15,22 @@ export function useImagePreview ({ target, file, previewUrl }: useImagePreviewOp
   } */
   if (file.value?.type?.match(/^image\/(jpeg|jpg|png|gif|webp|avif)$/)) {
     // (FileReaderオブジェクトを作成します)
-    const reader = new FileReader()
-    // (読み込みが完了したら処理が実行されます)
-    reader.onload = function () {
-      // (読み込んだファイルの内容を取得して変数imageに代入します)
-      const image: string | ArrayBuffer | null = reader.result
-      // console.log(image?.toString())
-      previewUrl.value = image?.toString()
-    }
-    // (DataURIScheme文字列を取得します)
-    // if (blob) reader.readAsDataURL(blob)
-    reader.readAsDataURL(file.value)
+    setImage({ file, previewUrl })
   } else {
     previewUrl.value = null
   }
+}
+
+const setImage = ({ file, previewUrl }: { file: Ref<File | undefined| null>, previewUrl: Ref<string | null | undefined> }): void => {
+  const reader = new FileReader()
+  // (読み込みが完了したら処理が実行されます)
+  reader.onload = function () {
+    // (読み込んだファイルの内容を取得して変数imageに代入します)
+    const image: string | ArrayBuffer | null = reader.result
+    // console.log(image?.toString())
+    previewUrl.value = image?.toString()
+  }
+  // (DataURIScheme文字列を取得します)
+  // if (blob) reader.readAsDataURL(blob)
+  if (file.value) reader.readAsDataURL(file.value)
 }
