cad836d466fccb6ba8df52a771099223c17fde52
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-03 08:46:03 +0900

    feat(composable): add util/use-date-util

---
+++ app/composables/util/use-date-util.ts
@@ -0,0 +1,7 @@
+export function useDateUtil() {
+  const isValidDate = (str: string): boolean => {
+    return !!str.match(/\d{4}\/\d{2}\/\d{2}/) && !isNaN(Date.parse(str))
+  }
+
+  return { isValidDate }
+}
