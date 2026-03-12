268970b143b187e78d4765fdffb153bb5ed8d20c
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-12 12:53:22 +0900

    refactor(composable): Refactor use-date

--- layers/voyage-nuxt/app/composables/util/use-date.ts
+++ layers/voyage-nuxt/app/composables/util/use-date.ts
@@ -1,6 +1,6 @@
 export function useDate() {
   const isValidDate = (str: string): boolean => {
-    return !!str.match(/\d{4}\/\d{2}\/\d{2}/) && !isNaN(Date.parse(str))
+    return !!str && !!str.match(/\d{4}\/\d{2}\/\d{2}/) && !isNaN(Date.parse(str))
   }
 
   return { isValidDate }
