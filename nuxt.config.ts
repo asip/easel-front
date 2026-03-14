2274416791506aca68e44813505cb37f1f95910e
Yasumasa Ashida <ys.ashida@gmail.com>
2026-03-14 17:41:40 +0900

    fix: Fix nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   modules: ['@nuxtjs/i18n', '@nuxt/test-utils', '@nuxt/eslint', '@regle/nuxt', 'vue-sonner/nuxt'],
   plugins: [],
-  extends: ['@voyage/nuxt'],
+  extends: ['./layers/voyage-nuxt'],
 
   i18n: {
     locales: [
