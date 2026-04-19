436686dd846ee370e8543614af175b17f136f6a4
Yasumasa Ashida <ys.ashida@gmail.com>
2026-04-19 20:09:21 +0900

    refactor(composable): Refactor ui/use-modal

--- app/composables/ui/use-modal.ts
+++ app/composables/ui/use-modal.ts
@@ -1,15 +1,19 @@
 export const useModal = function () {
   const modals: Partial<Record<string, HTMLDialogElement>> = {}
 
-  const openModal = (selector: string): void => {
+  const getModal = (selector: string): HTMLDialogElement | null => {
     const modalEl: HTMLDialogElement | null = modals[selector] ?? document.querySelector(selector)
     if (modalEl && !modals[selector]) modals[selector] = modalEl
+    return modalEl
+  }
+
+  const openModal = (selector: string): void => {
+    const modalEl: HTMLDialogElement | null = getModal(selector)
     modalEl?.showModal()
   }
 
   const closeModal = (selector: string): void => {
-    const modalEl: HTMLDialogElement | null = modals[selector] ?? document.querySelector(selector)
-    if (modalEl && !modals[selector]) modals[selector] = modalEl
+    const modalEl: HTMLDialogElement | null = getModal(selector)
     modalEl?.close()
   }
 
@@ -20,8 +24,7 @@ export const useModal = function () {
     modalEl: HTMLDialogElement | null
     isOutside: boolean
   } => {
-    const modalEl: HTMLDialogElement | null = modals[selector] ?? document.querySelector(selector)
-    if (modalEl && !modals[selector]) modals[selector] = modalEl
+    const modalEl: HTMLDialogElement | null = getModal(selector)
     const modalBoxEl: HTMLDivElement | null | undefined = modalEl?.querySelector('.modal-box')
     const rect = modalBoxEl?.getBoundingClientRect()
     const isOutside = rect
