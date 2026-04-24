5c5f3046da815c2705ded504ab1dd88e71b82b5e
Yasumasa Ashida <ys.ashida@gmail.com>
2026-04-24 13:03:05 +0900

    refactor(component): Refactor TagEdit

--- app/components/TagEdit.vue
+++ app/components/TagEdit.vue
@@ -1,27 +1,9 @@
 <script lang="ts" setup>
 const model = defineModel<string[]>()
 
-const { searchTag, tags } = useTagSearch()
-
-const tagEditor = useTemplateRef('tagEditorRef')
-const { initTagEditor, closeTagEditor } = useTagEditor({
-  el: tagEditor,
-  tagList: model,
-  tagSearch: { tags, searchTag },
-})
-
-onMounted(() => {
-  // console.log(frame)
-  if (import.meta.client && model.value) {
-    initTagEditor()
-  }
-})
-
-onUnmounted(() => {
-  if (import.meta.client) closeTagEditor()
-})
+const tagSearch = useTagSearch()
 </script>
 
 <template>
-  <input ref="tagEditorRef" type="text" value="" class="input h-auto" >
+  <TagEditor v-model="model" :tag-search="tagSearch" />
 </template>
---
+++ app/components/TagEditor.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+interface TagSearchType {
+  searchTag: (name: string, { signal }: { signal: AbortSignal }) => Promise<void>
+  tags: Ref<string[] | undefined>
+}
+
+const model = defineModel<string[]>()
+
+const { tagSearch } = defineProps<{
+  tagSearch: TagSearchType
+}>()
+
+const tagEditor = useTemplateRef('tagEditorRef')
+const { initTagEditor, closeTagEditor } = useTagEditor({
+  el: tagEditor,
+  tagList: model,
+  tagSearch,
+})
+
+onMounted(() => {
+  // console.log(frame)
+  if (import.meta.client && model.value) {
+    initTagEditor()
+  }
+})
+
+onUnmounted(() => {
+  if (import.meta.client) closeTagEditor()
+})
+</script>
+
+<template>
+  <input ref="tagEditorRef" type="text" value="" class="input h-auto" >
+</template>
