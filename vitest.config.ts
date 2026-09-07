a48d3dbada67b4dc1cabb6d3bd0ca76b78586be9
Yasumasa Ashida <ys.ashida@gmail.com>
2026-09-07 20:02:01 +0900

    refactor(test): Move around test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineVitestConfig({
   // any custom Vitest config you require
   test: {
     environment: 'nuxt',
-    dir: 'app/tests',
+    // dir: 'app',
     env: {
       BASE_URL: 'http://localhost:3000/api/v1',
     },
